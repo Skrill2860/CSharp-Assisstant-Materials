5eb22a758bb08821d084dc12f695a26157ef6e54
Skrill1945 <kasyabasya@mail.ru>
2022-10-06 14:38:26 +0300

    Initial commit

---
+++ SR_2 examples for 10/SR_2 example for 10 var 14/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace SR_2_example_for_10_var_14
+{
+    /*
+     Вариант 14 (нечетное число букв в имени) 
+
+    На вход программе подаются числа х, y и m.  
+
+    Необходимо написать метод, возвращающий сумму простых чисел и количество чисел,
+    делящихся на m в промежутке [x, y] через выходные параметры (out). 
+
+    При несоответствии вводимых пользователем данных спецификации и здравой логике
+    необходимо выводить осмысленное сообщение об ошибке и требовать повторить ввод.
+    Требуется организовать повтор решения задачи. 
+    Предусмотреть обработку исключительных ситуаций. 
+     */
+    class Program
+    {
+        /// <summary>
+        /// Проверяет число на простоту.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns>True если число простое, иначе false.</returns>
+        static bool IsPrime(int a)
+        {
+            if (a < 2)
+            {
+                return false;
+            }
+            for (int i = 2; i <= Math.Sqrt(a); i++)
+            {
+                if (a % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        // Это тоже стоит по логике разбить на 2 метода, о том что у нас метод выполняет
+        // не одну функцию явно намекает And, которое так и просится в название метода
+        /// <summary>
+        /// Подсчитывает сумму простых чисел и количество чисел делящихся на <paramref name="m"/> на промежутке [<paramref name="x"/>, <paramref name="y"/>].
+        /// </summary>
+        static void CalculatePrimeSumAndDivisibleCount(int x, int y, int m, out long primeSum, out int divisibleCount)
+        {
+            primeSum = 0;
+            divisibleCount = 0;
+            for (int i = x; i <= y; i++)
+            {
+                if (IsPrime(i))
+                {
+                    primeSum += i;
+                }
+                if (i % m == 0)
+                {
+                    divisibleCount++;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            bool repeat = true;
+            while (repeat)
+            {
+                Console.WriteLine("Введите начало промежутка:");
+                int begin, end, delimeter;
+                while (!int.TryParse(Console.ReadLine(), out begin) || begin < 0)
+                {
+                    Console.WriteLine("Введенное число некорректно. Начало промежутка - целое неотрицательное число.");
+                }
+
+                Console.WriteLine("Введите конец промежутка:");
+                while (!int.TryParse(Console.ReadLine(), out end) || end < begin)
+                {
+                    Console.WriteLine("Введенное число некорректно. Конец промежутка - целое неотрицательное число. " +
+                        "Конец должен быть больше либо равен началу.");
+                }
+
+                Console.WriteLine("Введите m:");
+                while (!int.TryParse(Console.ReadLine(), out delimeter) || delimeter <= 0)
+                {
+                    Console.WriteLine("Введенное число некорректно. Введите натуральное число.");
+                }
+
+                CalculatePrimeSumAndDivisibleCount(begin, end, delimeter, out long primeSum, out int divCount);
+                Console.WriteLine($"На промежутке [{begin}, {end}]:\nСумма простых чисел: {primeSum}\n" +
+                    $"Количество чисел, делящихся на m: {divCount}");
+                
+                Console.WriteLine("Хотите начать заново? (y/n)");
+                string s = Console.ReadLine().ToLower();
+                if (s == "no" || s == "n")
+                {
+                    repeat = false;
+                }
+            }
+        }
+    }
+}
---
+++ SR_2 examples for 10/SR_2 examples for 10 var 10/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace SR_1_example_for_10_var_10
+{
+    /*
+    Вариант 10  (четное число букв в имени) 
+
+    Программа запрашивает у пользователя два целочисленных значения a и A
+    (a < A) – минимальное и максимальное значение длины стороны правильно треугольника, соответственно.
+    Затем, выводит на экран значение площади треугольника, периметра треугольника и
+    отношение площади треугольника к его периметру для длин сторон из интервала
+    [a, A] с шагом 1 (текущее значение стороны также вывести на экран).  
+
+    Нахождение периметра треугольника и площади необходимо вынести в отдельный метод,
+    который принимает в качестве параметров текущее значение длины стороны и возвращает 
+    площадь треугольника и его периметр через выходные параметры (out). 
+    Площадь треугольника и его периметр представляются вещественными числами. 
+
+    При несоответствии вводимых пользователем данных спецификации и здравой 
+    логике необходимо выводить осмысленное сообщение об ошибке и требовать 
+    повторить ввод. Требуется организовать повтор решения задачи. 
+    Предусмотреть обработку исключительных ситуаций. 
+     */
+    class Program
+    {
+        // В условии просят один метод который считает и то, и то,
+        // но логичнее будет разделить его на два разных
+
+        /// <summary>
+        /// Высчитывает периметр равнобедренного треугольника по длине его стороны <paramref name="sideLength"/>.
+        /// </summary>
+        /// <param name="sideLength">Длина стороны</param>
+        /// <param name="perimeter">Значение периметра равнобедренного треугольника</param>
+        static void CalculatePerimeter(int sideLength, out int perimeter)
+        {
+            perimeter = 3 * sideLength;
+        }
+
+        /// <summary>
+        /// Высчитывает площадь равнобедренного треугольника по длине его стороны <paramref name="sideLength"/>.
+        /// </summary>
+        /// <param name="sideLength">Длина стороны</param>
+        /// <param name="area">Значение площади равнобедренного треугольника</param>
+        static void CalculateArea(int sideLength, out double area)
+        {
+            // Вроде у площади такая формула
+            area = sideLength * sideLength * Math.Sqrt(3) / 2;
+        }
+
+        static void Main(string[] args)
+        {
+            bool repeat = true;
+            while (repeat)
+            {
+                Console.WriteLine("Введите минимальную длину стороны:");
+                int minLength, maxLength;
+                while (!int.TryParse(Console.ReadLine(), out minLength) || minLength < 0)
+                {
+                    Console.WriteLine("Введеная минимальная длина некорректна. Длина - целое неотрицательное число");
+                }
+
+                Console.WriteLine("Введите максимальную длину стороны:");
+                while (!int.TryParse(Console.ReadLine(), out maxLength) || maxLength < minLength)
+                {
+                    Console.WriteLine("Введеная максимальная длина некорректна. Длина - целое неотрицательное число." +
+                        " Макс длина должна быть больше либо равна мин длине.");
+                }
+
+                for (int i = minLength; i <= maxLength; i++)
+                {
+                    CalculatePerimeter(i, out int perimeter);
+                    CalculateArea(i, out double area);
+                    Console.WriteLine($"Для стороны длины {i}:\n" +
+                        $"Периметр: {perimeter}\nПлощадь: {area}\nОтношение: {area / (perimeter == 0 ? 1 : perimeter)}\n");
+                }
+                Console.WriteLine("Хотите начать заново? (y/n)");
+                string s = Console.ReadLine().ToLower();
+                if (s == "no" || s == "n")
+                {
+                    repeat = false;
+                }
+            }
+        }
+    }
+}
